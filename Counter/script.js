b7102c35e7ff009c4efca310a1a3f4b28314f664
Prajwal Ghatole <155899226+Prajwal-ETH@users.noreply.github.com>
2024-02-26 19:34:56 +0530

    script.js

---
+++ Counter/script.js
@@ -0,0 +1,23 @@
+const count = document.querySelector('.count')
+const decrementBtn = document.querySelector('.decrement')
+const incrementBtn = document.querySelector('.increment')
+const changeBy = document.querySelector('#changeBy')
+const resetBtn = document.querySelector('.reset-btn')
+
+
+decrementBtn.addEventListener('click', () => {
+    const countVal = parseInt(count.innerText)
+    const changeByValue = parseInt(changeBy.value)
+    count.innerText = countVal - changeByValue
+})
+
+
+incrementBtn.addEventListener('click', () => {
+    const countVal = parseInt(count.innerText)
+    const changeByValue = parseInt(changeBy.value)
+    count.innerText = countVal + changeByValue
+})
+
+resetBtn.addEventListener('click', () => {
+    count.innerText = '0'
+})

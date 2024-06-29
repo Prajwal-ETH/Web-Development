270a093a13ccb40bb77eb9504ce9756d396dd9f9
Prajwal Ghatole <155899226+Prajwal-ETH@users.noreply.github.com>
2024-06-29 23:44:48 +0530

    script.js

--- Drag & Drop/script.js
+++ Drag & Drop/script.js
@@ -2,7 +2,6 @@ let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
-
 for(list of lists){
     list.addEventListener("dragstart" , function(e){
         let selected = e.target;
